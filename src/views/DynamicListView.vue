ed1f7819a390d8fcc3edf34b0b59394fd28a41af
Bibiano Ruiz <B.Ruiz@skills-assured.com>
2022-09-08 17:37:02 +0200

    Add edit mode to dynamic list

--- src/views/DynamicListView.vue
+++ src/views/DynamicListView.vue
@@ -6,6 +6,8 @@ import { ref, computed } from 'vue';
   const hideCompleted = ref(false);
   const newElement = ref('');
   const newHighPriority = ref(false);
+  const editing = ref(false);
+
   const elements = ref([
     { id: id++, text: 'Learn HTML', highPriority: false, done: true },
     { id: id++, text: 'Learn CSS', highPriority: false, done: false },
@@ -32,14 +34,28 @@ import { ref, computed } from 'vue';
   function removeElement(element) {
     elements.value = elements.value.filter(x => x != element);
   }
-
+  const doEdit = (e) => {
+    editing.value = e
+    newElement.value = ''
+  }
 </script>
 
 <template>
-  <form @submit.prevent="addElement">
-    <input v-model="newElement">
+  <div class="header">
+    <h1>This is a dynamic list</h1>
+    <button v-if="editing" class="" @click="doEdit(false)">
+        Cancel
+    </button>
+    <button v-else class="" @click="doEdit(true)">
+      Edit mode
+</button>
+  </div>
+  <form 
+  v-if="editing"
+  @submit.prevent="addElement">
+    <input v-model="newElement" placeholder="Add new element to the list...">
     <label>
-      <input type="checkbox" v-model="newHighPriority" >
+      <input type="checkbox" v-model="newHighPriority">
       High Priority
     </label>
     <button>Add Element</button>
@@ -48,7 +64,7 @@ import { ref, computed } from 'vue';
     <li 
       v-for="element in filteredElements" 
       :key="element.id"
-      @click="element.done = !element.done"
+      @click="element.done = !element.done && editing"
     >
       <span 
         class="element__list"
@@ -56,9 +72,27 @@ import { ref, computed } from 'vue';
       >
         {{ element.text }}
       </span>
-      <button @click="removeElement(element)">Delete</button>
+      <button @click="removeElement(element)" v-if="editing">Delete</button>
     </li>
   </ul>
+  <!-- <ul>
+    <li 
+      v-for="{id, text, highPriority, done} in filteredElements" 
+      :key="id"
+      @click="done = !done"
+    >
+      <span 
+        class="element__list"
+        :class="[{ done: done}, { highPriority: highPriority}]"
+      >
+        {{ text }}
+      </span>
+      <button @click="removeElement({id: id, text: text, highPriority: highPriority, done: done})" v-if="editing">Delete</button>
+    </li>
+  </ul> -->
+  <p v-if="!filteredElements.length">
+    Nothing to see here
+  </p>
   <div class="button__hide">
     <button @click="hideCompleted = !hideCompleted">
       {{ hideCompleted ? 'Show all' : 'Hide completed'}}
@@ -77,7 +111,7 @@ import { ref, computed } from 'vue';
   form {
     display: flex;
     justify-content: space-evenly;
-}
+  }
   .done {
     text-decoration: line-through;
     color: gray;
@@ -93,4 +127,14 @@ import { ref, computed } from 'vue';
     text-align: center;
     margin-top: 20px;
   }
+  .header {
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 20px;
+    }
+
+    .header button {
+        padding: 15px 0;
+        background-color: antiquewhite;
+      }
 </style>
